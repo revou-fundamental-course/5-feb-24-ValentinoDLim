41d93b1910f7f6e44b43dc3aa27b0f7e8837d3f1
unknown <valentinodemon12@gmail.com>
2024-02-14 22:50:27 +0700

    first push

--- js/script.js
+++ js/script.js
@@ -3,18 +3,8 @@ function updateSuhu() {
     var suhuInput = parseFloat(document.getElementById("suhu").value);
     var suhu;
 
-    // Konversi suhu sesuai dengan unit yang dipilih
-    if (unit === "celcius") {
-        suhu = (suhuInput - 32) * 5 / 9;
-    } else if (unit === "fahrenheit") {
-        suhu = (suhuInput * 9 / 5) + 32;
-    }
-
-    // Update nilai suhu
-    document.getElementById("suhu").value = suhu;
-
-    // Panggil fungsi reverse
-    reverse();
+    convert();
+    reset();
 }
 
 
@@ -24,15 +14,18 @@ function convert() {
     var hasil = document.getElementById("hasil");
     var rumus = document.getElementById("rumus");
     var penjelasan = document.getElementById("penjelasan");
+    var reversean = document.getElementById("reversean");
 
     if (unit === "celcius") {
         hasil.value = (suhu * 9 / 5) + 32;
-        rumus.value = "(" + suhu + " - 32) x 5/9 = " + hasil.value;
+        rumus.value = "(" + suhu + " x 9/5) + 32 = " + hasil.value;
         penjelasan.innerText = "Untuk mengkonversi suhu dari Celsius ke Fahrenheit, Anda dapat menggunakan rumus konversi F = 9/5C + 32, di mana F adalah suhu dalam Fahrenheit dan C adalah suhu dalam Celsius. Langkah-langkahnya adalah mengalikan suhu dalam Celsius dengan 9/5, lalu menambahkan 32.";
+        reversean.innerText = "fahrenheit";
     } else if (unit === "fahrenheit") {
         hasil.value = (suhu - 32) * 5 / 9;
-        rumus.value = "(" + suhu + " x 9/5) + 32 = " + hasil.value;
+        rumus.value = "(" + suhu + " - 32) x 5/9 = " + hasil.value;
         penjelasan.innerText = "Untuk mengkonversi suhu dari Fahrenheit ke Celsius, Anda dapat menggunakan rumus C = 5 9 ( F - 32 ) , di mana C adalah suhu dalam Celsius dan F adalah suhu dalam Fahrenheit. Langkah-langkahnya adalah mengalikan suhu dalam Fahrenheit dengan 5/9, kemudian mengurangkan 32 dari hasilnya untuk mendapatkan suhu dalam Celsius.";
+        reversean.innerText = "celcius";
     }
 }
 
@@ -41,6 +34,7 @@ function reset() {
     document.getElementById("hasil").value = "";
     document.getElementById("rumus").value = "";
     document.getElementById("penjelasan").innerText = "";
+    document.getElementById("reversean").innerText = "";
 }
 
 function reverse() {
@@ -48,28 +42,18 @@ function reverse() {
     var suhu = parseFloat(document.getElementById("suhu").value);
     var hasil = document.getElementById("hasil");
     var rumus = document.getElementById("rumus");
-    var penjelasan = document.getElementById("penjelasan");
+    var reversean = document.getElementById("reversean");
 
     if (unit === "celcius") {
-        if (rumus.value.includes("-")) {
-            hasil.value = (suhu * 9 / 5) + 32;
-            rumus.value = "(" + suhu + " - 32) x 5/9 = " + hasil.value;
-            penjelasan.innerText = "Untuk mengkonversi suhu dari Fahrenheit ke Celsius, Anda dapat menggunakan rumus C = 5/9 (F - 32), di mana C adalah suhu dalam Celsius dan F adalah suhu dalam Fahrenheit. Langkah-langkahnya adalah mengurangkan 32 dari suhu dalam Fahrenheit, kemudian mengalikan hasilnya dengan 5/9 untuk mendapatkan suhu dalam Celsius.";
-        } else {
-            hasil.value = (suhu - 32) * 5 / 9;
-            rumus.value = "(" + suhu + " x 9/5) + 32 = " + hasil.value;
-            penjelasan.innerText = "Untuk mengkonversi suhu dari Celsius ke Fahrenheit, Anda dapat menggunakan rumus konversi F = 9/5C + 32, di mana F adalah suhu dalam Fahrenheit dan C adalah suhu dalam Celsius. Langkah-langkahnya adalah mengalikan suhu dalam Celsius dengan 9/5, lalu menambahkan 32.";
-        }
+        hasil.value = (suhu - 32) * 5 / 9;
+        rumus.value = "(" + suhu + " - 32) x 5/9 = " + hasil.value;
+        reversean.innerText = "celcius";
+        penjelasan.innerText = "Untuk mengkonversi suhu dari Fahrenheit ke Celsius, Anda dapat menggunakan rumus C = 5 9 ( F - 32 ) , di mana C adalah suhu dalam Celsius dan F adalah suhu dalam Fahrenheit. Langkah-langkahnya adalah mengalikan suhu dalam Fahrenheit dengan 5/9, kemudian mengurangkan 32 dari hasilnya untuk mendapatkan suhu dalam Celsius.";
     } else if (unit === "fahrenheit") {
-        if (rumus.value.includes("x")) {
-            hasil.value = (suhu - 32) * 5 / 9;
-            rumus.value = "(" + suhu + " x 9/5) + 32 = " + hasil.value;
-            penjelasan.innerText = "Untuk mengkonversi suhu dari Fahrenheit ke Celsius, Anda dapat menggunakan rumus C = 5/9 (F - 32), di mana C adalah suhu dalam Celsius dan F adalah suhu dalam Fahrenheit. Langkah-langkahnya adalah mengurangkan 32 dari suhu dalam Fahrenheit, kemudian mengalikan hasilnya dengan 5/9 untuk mendapatkan suhu dalam Celsius.";
-        } else {
-            hasil.value = (suhu * 9 / 5) + 32;
-            rumus.value = "(" + suhu + " - 32) x 5/9 = " + hasil.value;
-            penjelasan.innerText = "Untuk mengkonversi suhu dari Celsius ke Fahrenheit, Anda dapat menggunakan rumus konversi F = 9/5C + 32, di mana F adalah suhu dalam Fahrenheit dan C adalah suhu dalam Celsius. Langkah-langkahnya adalah mengalikan suhu dalam Celsius dengan 9/5, lalu menambahkan 32.";
-        }
+        hasil.value = (suhu * 9 / 5) + 32;
+        rumus.value = "(" + suhu + " x 9/5) + 32 = " + hasil.value;
+        reversean.innerText = "fahrenheit";
+        penjelasan.innerText = "Untuk mengkonversi suhu dari Celsius ke Fahrenheit, Anda dapat menggunakan rumus konversi F = 9/5C + 32, di mana F adalah suhu dalam Fahrenheit dan C adalah suhu dalam Celsius. Langkah-langkahnya adalah mengalikan suhu dalam Celsius dengan 9/5, lalu menambahkan 32.";
     }
 }
 
